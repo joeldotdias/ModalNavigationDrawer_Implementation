af99c50e3aa7a92e8141286c4134f3fa76f85728
Joel Dias <joeldias0204@gmail.com>
2023-11-14 19:38:19 +0530

    Final implementation

--- app/src/main/java/com/example/navigationdrawerimpl/NavDrawer.kt
+++ app/src/main/java/com/example/navigationdrawerimpl/NavDrawer.kt
@@ -22,11 +22,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
